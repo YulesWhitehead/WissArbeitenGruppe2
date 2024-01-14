b5ce3cdd629747b74dd0c344710e5164bd1d5616
maikedu <154339604+maikedu@users.noreply.github.com>
2024-01-14 19:08:08 +0100

    Update Einlesen.R

--- Einlesen.R
+++ Einlesen.R
@@ -13,3 +13,47 @@ anrede <- function(x) {
 }
 # Anwendung auf die Daten und erstellung eines neuen Vektors für die Anreden
 data$Anrede <- anrede(data$Name)
+
+## Extrahieren der einzigartigen Anreden
+
+unique(data$Anrede)
+# [1] "Mr"           "Mrs"          "Miss"         "Master"       "Don"         
+# [6] "Rev"          "Dr"           "Mme"          "Ms"           "Major"       
+# [11] "Lady"         "Sir"          "Mlle"         "Col"          "Capt"        
+# [16] "the Countess" "Jonkheer" 
+
+## Zusammenfassen gleichbedeutender Anreden
+comp <- function(x) {
+  for (i in 1:length(x)) {
+    if (x[i] == "Miss") {
+      x[i] <- "Ms"
+    }
+    else if (x[i] == "Mile") {
+      x[i] <- "Ms"
+    }
+    else if (x[i] == "Mme") {
+      x[i] <- "Mrs"
+    }
+    else if (x[i] == "Don") {
+      x[i] <- "Mr"
+    }
+  }
+  return(x)
+}
+
+data$Anrede <- comp(data$Anrede)
+## Die anderen Anreden sind inhaltlich nicht exakt gleich und es ist zu
+## erwarten das die betroffenen Personen sich jeweils in wichtigen 
+## Attributen unterscheide (Durchschnittsalter, etc)
+
+## Faktorisierung von "Survived"
+
+data$Survived <- factor(data$Survived, 0:1, c("No", "Yes"))
+
+## Faktorisierung von "Sex"
+
+data$Sex <- factor(data$Sex, c("male", "female"), c("Male", "Female"))
+
+## Faktorisierung von "Embarked"
+
+data$Embarked <- factor(data$Embarked, c("C", "Q", "S"), c("Cherbourg", "Queenstown", "Southampton"))
