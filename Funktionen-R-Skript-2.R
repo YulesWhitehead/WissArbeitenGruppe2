ba2e0925fe1fae1351d1120ed48a552792868cbc
KatrinBerger <119357325+KatrinBerger@users.noreply.github.com>
2024-02-20 10:45:40 +0100

    Update Funktionen-R-Skript-2.R

--- Funktionen-R-Skript-2.R
+++ Funktionen-R-Skript-2.R
@@ -13,3 +13,22 @@ CramersV <- function(Merkmal1, Merkmal2){
   Chiquadrat <- as.numeric(Liste[1])
   sqrt(Chiquadrat/(n*m))
 }
+
+# normKontingenz -  Funktion zur Berechnung des normierten
+#                   Kontingenzkoeffizienten nach Pearson
+#
+# Eingabe:  zwei Vektoren mit Auspraegungen von kategorialen Variablen gleicher
+#           Laenge
+#
+# Ausgabe:  normierter Kontingenzkoeffizient nach Pearson
+
+normKontingenz <- function(Merkmal1, Merkmal2){
+  Tabelle <- table(Merkmal1, Merkmal2)
+  m <- min(ncol(Tabelle), nrow(Tabelle))
+  n <- length(Merkmal1)
+  Liste <- chisq.test(Merkmal1, Merkmal2)
+  Chiquadrat <- as.numeric(Liste[1])
+  Kontingenz <- sqrt(Chiquadrat/(Chiquadrat + n))
+  Korrektur <- sqrt((m-1)/m)
+  Kontingenz/Korrektur
+}
