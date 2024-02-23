4dc3d0bf121201cebfccb46a4cccc56342eba43f
SofiiSoz <148062352+SofiiSoz@users.noreply.github.com>
2024-02-23 15:52:25 +0100

    Update Deskription-Visualisierung.R
    Minimum zu den Funktionen (iii) und (iv)

--- Deskription-Visualisierung.R
+++ Deskription-Visualisierung.R
@@ -5,3 +5,9 @@ str(data)
 metr_stat(data[,5:8])
 
 kateg_stat(data[,c(2:4,9:12)])
+
+# a-iii Zusammenhang zwischen zwei kategorialen Variablen (z.B. Geschlecht und Überlebensstatus)
+zusammenhang_kat_var <- kat_stat(data$Sex, data$Survived)
+
+# a-iv Zusammenhang zwischen einer metrischen und einer dichotomen Variable (z.B. Alter und Überlebensstatus)
+zusammenhang_met_dich <- analyzeMetricDichotomous(data$Age, data$Survived)
