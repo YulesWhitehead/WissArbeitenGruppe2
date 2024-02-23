634957d5961be9bc29858b75b12f6c78056112c8
Alona Predkova <alena.predkova21@gmail.com>
2024-02-23 12:07:36 +0100

    Update Funktionen-R-Skript-1.R

--- Funktionen-R-Skript-1.R
+++ Funktionen-R-Skript-1.R
@@ -15,9 +15,9 @@ library(e1071)
 
 metr_stat <- function(data) {
   # Erstellen einer leeren Ergebnismatrix
-  result <- matrix(NA, nrow = length(data), ncol = 15)
-  colnames(result) <- c("Variable", "Anzahl", "Minimum", "Maximum", "Spannweite",
-                        "Median", "Erste Quartil", "Letzte Quartil", 
+  result <- matrix(NA, nrow = length(data), ncol = 16)
+  colnames(result) <- c("Variable", "Anzahl", "NA-Anzahl", "Minimum", "Maximum", 
+                        "Spannweite", "Median", "Erste Quartil", "Letzte Quartil", 
                         "Interquartilsabstand", "Arithm. Mittel", 
                         "Varianz", "Standardabw.", "Variationskoef.",
                         "Schiefe", "Kurtosis")
@@ -36,19 +36,20 @@ metr_stat <- function(data) {
     # Speichern der berechneten Statistiken in die Ergebnismatrix
     result[i, 1] <- var_name
     result[i, 2] <- length(var_data)
-    result[i, 3] <- Min
-    result[i, 4] <- Max
-    result[i, 5] <- Max - Min
-    result[i, 6] <- median(var_data)
-    result[i, 7] <- q1
-    result[i, 8] <- q2
-    result[i, 9] <- q2 - q1
-    result[i, 10] <- Mean
-    result[i, 11] <- var(var_data)
-    result[i, 12] <- Sd
-    result[i, 13] <- Sd / Mean
-    result[i, 14] <- skewness(var_data)
-    result[i, 15] <- kurtosis(var_data)
+    result[i, 3] <- sum(is.na(var_data))
+    result[i, 4] <- Min
+    result[i, 5] <- Max
+    result[i, 6] <- Max - Min
+    result[i, 7] <- median(var_data)
+    result[i, 8] <- q1
+    result[i, 9] <- q2
+    result[i, 10] <- q2 - q1
+    result[i, 11] <- Mean
+    result[i, 12] <- var(var_data)
+    result[i, 13] <- Sd
+    result[i, 14] <- Sd / Mean
+    result[i, 15] <- skewness(var_data)
+    result[i, 16] <- kurtosis(var_data)
   }
   
   # Rueckgabe der Ergebnismatrix als Dataframe
@@ -58,35 +59,44 @@ metr_stat <- function(data) {
 
 #a-ii
 
-# metr_stat - Funktion zur Berechnung deskriptiver Statistiken fuer kategoriale
-#             Variablen
+# kateg_stat - Funktion zur Berechnung deskriptiver Statistiken fuer kategoriale
+#              Variablen
 # 
 # Eingabe: 
-# data      - Datensatz, fuer den die Statistiken berechnet werden sollen
+# data       - Datensatz, fuer den die Statistiken berechnet werden sollen
 #
 # Ausgabe:
-# result    - Ergebnismatrix als Dataframe mit allen berechneten Statistiken
+# result     - Ergebnismatrix als Dataframe mit allen berechneten Statistiken
 
 kateg_stat <- function(data){
   # Erstellen einer leeren Ergebnismatrix
-  result <- matrix(NA, nrow = length(data), ncol = 3)
-  colnames(result) <- c("Variable", "Anzahl", "Modalwert")
+  result <- matrix(NA, nrow = ncol(data), ncol = 5)
+  colnames(result) <- c("Variable", "Anzahl", "NA-Anzahl", "Modalwert", 
+                        "Relative Haeufigkeit")
   
   # Iteration über die Variablen im Datensatz
-  for(i in 1:length(data)) {
+  for(i in 1:ncol(data)) {
     var_name <- names(data)[i]  
     var_data <- data[[i]]
+    
+    # Berechnung der Modalwert
+    modalwert <- moda(var_data)
+    
+    # Berechnung der relativen Haeufigkeit
+    rel_h <- table(var_data) / length(var_data)
     
     # Speichern der berechneten Statistiken in die Ergebnismatrix
     result[i, 1] <- var_name
     result[i, 2] <- length(var_data)
-    result[i, 3] <- moda(var_data)
+    result[i, 3] <- sum(is.na(var_data))
+    result[i, 4] <- modalwert
+    result[i, 5] <- rel_h[as.character(modalwert)] 
+    # Nur die relative Haeufigkeit des Modalwerts speichern
   }
   
-  # Rueckgabe der Ergebnismatrix als Dataframe
+  # Rückgabe der Ergebnismatrix als Dataframe
   return(as.data.frame(result))
 }
-
 
 #a-iii
 
