6eaad060fec58ad3616b99e2d9a02f58c918dd42
Alona Predkova <alena.predkova21@gmail.com>
2024-02-23 20:15:16 +0100

    Update Deskription-Visualisierung.R

--- Deskription-Visualisierung.R
+++ Deskription-Visualisierung.R
@@ -1,10 +1,13 @@
 attach(data)
 
+#Ueberblick von Daten und deren Auspraegungen
 str(data)
 
-metr_stat(data[,5:8])
+# Berechnung von deskriptiven Statistiken fuer metrische Variablen
+metr_stat(data[,c(1:4, 5:8)])
 
-kateg_stat(data[,c(2:4,9:12)])
+# Berechnung von deskriptiven Statistiken fuer kategoriale Variablen
+kateg_stat(data[,9:12])
 
 # a-iii Zusammenhang zwischen zwei kategorialen Variablen (z.B. Geschlecht und Überlebensstatus)
 zusammenhang_kat_var <- kat_stat(data$Sex, data$Survived)
