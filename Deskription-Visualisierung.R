7ea3b573a0fb9519522c1a50c079043a7715e873
Alona Predkova <alena.predkova21@gmail.com>
2024-02-25 18:20:52 +0100

    Update Deskription-Visualisierung.R

--- Deskription-Visualisierung.R
+++ Deskription-Visualisierung.R
@@ -13,31 +13,48 @@ metr_stat(data_metrisch)
 # Berechnung von deskriptiven Statistiken fuer kategoriale Variablen
 kateg_stat(data_kategorial)
 
-# a-iii Zusammenhang zwischen zwei kategorialen Variablen (z.B. Geschlecht und Überlebensstatus)
-zusammenhang_Survived_Sex <- kat_stat(data$Survived, data$Sex)
+# a-iii Zusammenhang zwischen zwei kategorialen Variablen
+zusammenhang_Survived_Sex <- kat_stat(Survived, Sex)
 #Cramer  normKont 
 #0.5409359 0.6728632 
-zusammenhang_Survived_Pclass <- kat_stat(data$Survived, data$Pclass)
+#Diese Ergebnisse deuten auf einen mittleren bis hoeheren Zusammenhang zwischen 
+#Sex und Survived hin.
+
+zusammenhang_Survived_Pclass <- kat_stat(Survived, Pclass)
 #Cramer  normKont 
-#0.3398174 0.4550200 
-zusammenhang_Survived_Side <- kat_stat(data$Survived, data$Side)
+#0.3398174 0.4550200
+#Diese Ergebnisse deuten auf einen mittleren Zusammenhang zwischen 
+#Pclass und Survived hin.
+
+zusammenhang_Survived_Side <- kat_stat(Survived, Side)
 #Cramer   normKont 
 #0.05982927 0.08446033 
-zusammenhang_Survived_Deck <- kat_stat(data$Survived, data$Deck)
+#Diese Ergebnisse deuten darauf hin, dass es fast kein Zusammenhang zwischen 
+#Side und Survived gibt.
+
+zusammenhang_Survived_Deck <- kat_stat(Survived, Deck)
 #Cramer  normKont 
 #0.1182824 0.1661186
-zusammenhang_Survived_Anrede <- kat_stat(data$Survived, data$Anrede)
+#Diese Ergebnisse deuten auf einen schwachen Zusammenhang zwischen 
+#Pclass und Survived hin.
+
+zusammenhang_Survived_Anrede <- kat_stat(Survived, Anrede)
 #Cramer  normKont 
 #0.5796337 0.7092011
+#Diese Ergebnisse deuten auf einen mittleren bis hoeheren Zusammenhang zwischen 
+#Anrede und Survived hin.
 
-# a-iv Zusammenhang zwischen einer metrischen und einer dichotomen Variable (z.B. Alter und Überlebensstatus)
-zusammenhang_Survived_Age <- analyzeMetricDichotomous(data$Age, data$Survived)
-# p-Wert des t-Tests liegt bei 0,009
-zusammenhang_Survived_Fare <- analyzeMetricDichotomous(data$Fare, data$Survived)
+# a-iv Zusammenhang zwischen einer metrischen und einer dichotomen Variable
+zusammenhang_Survived_Age <- analyzeMetricDichotomous(Age, Survived)
+# p-Wert des t-Tests liegt bei 0,009279
+
+zusammenhang_Survived_Fare <- analyzeMetricDichotomous(Fare, Survived)
 # p-Wert des t-Tests liegt bei p-value = 2.699e-11
-zusammenhang_Survived_SibSp <- analyzeMetricDichotomous(data$SibSp, data$Survived)
+
+zusammenhang_Survived_SibSp <- analyzeMetricDichotomous(SibSp, Survived)
 # p-Wert des t-Tests liegt bei p-value = 0.2327
-zusammenhang_Survived_Parch <- analyzeMetricDichotomous(data$Parch, data$Survived)
+
+zusammenhang_Survived_Parch <- analyzeMetricDichotomous(Parch, Survived)
 # p-Wert des t-Tests liegt bei p-value = 0.01339
 
 
